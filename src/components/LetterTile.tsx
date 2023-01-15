280504685c452eabdbc3834c3b0cc9df055271a8
Daniel Redly <dredly@gmail.com>
2023-01-15 10:22:52 +0000

    Fixed styled component warning for dynamically setting rotation of letters

--- src/components/LetterTile.tsx
+++ src/components/LetterTile.tsx
@@ -1,24 +1,28 @@
 import styled from "styled-components"
 
-const LetterTile = ({ letter, rotation }: { letter: string, rotation: number }) => {
-    const tileStyles = `
-        box-sizing: border-box;
-        border: 1px solid #221d0c;
-        box-shadow: inset -0.1em -0.1em 0 0 #221d0c38;
-        background: #ccae4b;
-        width: 50px;
-        height: 50px;
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 40px;
-        cursor: pointer;
-        transform: rotate(${rotation}turn);
-    `
+interface Props {
+    rotation: number
+}
 
-    const Tile = styled.div`${tileStyles}`;
-    return <Tile><p>{letter}</p></Tile>
+const Tile = styled.div<Props>`
+    box-sizing: border-box;
+    border: 1px solid #221d0c;
+    box-shadow: inset -0.1em -0.1em 0 0 #221d0c38;
+    background: #ccae4b;
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 40px;
+    cursor: pointer;
+    transform: rotate(${props => props.rotation}turn);
+`;
+
+const LetterTile = ({ letter, rotation }: { letter: string, rotation: number }) => {
+
+    return <Tile rotation={rotation}><p>{letter}</p></Tile>
 }
 
 export default LetterTile
